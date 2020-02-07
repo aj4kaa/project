4ed0b76f33fc915fe4cf1bee0d69ca49933e42e7
aj4ka <aimfury@gmail.com>
2020-02-07 09:04:21 +0300

    finish model windows and mask form

--- src/js/script.js
+++ src/js/script.js
@@ -39,6 +39,51 @@ document.querySelector('.next').addEventListener('click', function () {
 		toggleSlide('.catalog-item__link');
 		toggleSlide('.catalog-item__back');
 
-	 
+		//MODAL
+
+		$('[data-modal=consultation]').on('click', function() {
+			$('.overlay, #consultation').fadeIn('slow');
+		});
+		$('.modal__close').on('click', function() {
+			$('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+		});
+		$('.button_mini').each(function(i) {
+			$(this).on('click', function() {
+				$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+				$('.overlay, #order').fadeIn('slow');
+
+			});
+		});
+
+		//VALIDATOR
+
+		function validateForms(form){
+			$(form).validate({
+				rules: {
+					name: "required",
+					phone: "required",
+					email: {
+						required: true,
+						email: true
+					}
+				},
+				messages: {
+					name: "Пожалуйста, введите своё имя",
+					phone: "Пожалуйста, введите свой номер телефона",
+					email: {
+					  required: "Пожалуйста, введите свою почту",
+					  email: "неправильно введен адрес почты"
+					}
+				  }
+			});
+	
+		};
+
+		validateForms('#consultation-form');
+		validateForms('#consultation form');
+		validateForms('#order form');
+
+		$('input[name=phone]').mask("+7 (999) 999-99-99", {selectOnFocus: true});
+		
 	});
 	})(jQuery);
